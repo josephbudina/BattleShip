dd09ee09b20e65a985346ad8f7d61b2f050b383c
Wil McCauley <wmccauley724@gmail.com>
2020-12-08 18:22:22 -0700

    Add test for valid placement comparing length

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,6 +7,8 @@ require './lib/board'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists_with_attributes
@@ -25,5 +27,10 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("E1")
 
     assert_equal false, @board.valid_coordinate?("A22")
+  end
+
+  def test_valid_placement_for_length_of_ship
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 end
