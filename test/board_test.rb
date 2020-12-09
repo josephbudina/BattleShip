14fad078d2385b3bbb2fd6255252b11c80d12083
Joseph Budina <joebud3@gmail.com>
2020-12-09 12:07:31 -0700

    Add letters and numbers consecutive tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -33,4 +33,40 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
+  
+  def test_it_can_split_letters
+    assert_equal [65, 65, 65], @board.split_letters(["A1", "A2", "A4"])
+  end
+  
+  def test_it_can_split_numbers
+    assert_equal [1, 2, 4], @board.split_numbers(["A1", "A2", "A4"])
+  end
+
+  def test_letters_consecutive
+    assert_equal true, @board.consecutive_letters?(["A1", "B2", "C4"])
+    assert_equal false, @board.consecutive_letters?(["A1", "C2", "C4"])
+  end
+  
+  def test_numbers_consecutive
+    assert_equal false, @board.consecutive_numbers?(["A1", "B2", "C4"])
+    assert_equal true, @board.consecutive_numbers?(["A1", "B2", "C3"])
+  end
+
+  def test_numbers_and_letters_consecutive
+    assert_equal false, @board.consecutive_letters_and_numbers?(["A1", "A2", "A4"])
+    assert_equal true, @board.consecutive_letters_and_numbers?(["A1", "A2", "A3"])
+
+    assert_equal false, @board.consecutive_letters_and_numbers?(["A1", "C1"])
+    assert_equal true, @board.consecutive_letters_and_numbers?(["A1", "B1"])
+  end
+
+  def test_valid_placement_must_be_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+  end
 end
