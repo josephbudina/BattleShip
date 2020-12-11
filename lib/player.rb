6ea6fdc432f66b06b06775a7014e1807137327b2
Joseph Budina <joebud3@gmail.com>
2020-12-11 10:05:15 -0700

    Add place ships method to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,17 @@
 class Player
-  attr_reader :ship_array
+  attr_reader :board,
+              :ship_array
 
   def initialize(board, ship_array)
     @board      = board
     @ship_array = ship_array
+  end
+
+  def place_ships(ship, coordinates)
+    if @board.valid_placement?(ship, coordinates)
+      @board.place(ship, coordinates)
+    else
+      "Cannot Place Ship"
+    end
   end
 end
