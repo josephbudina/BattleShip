0d5f056d67f3e17b1452443ba130c28923accd2c
Joseph Budina <joebud3@gmail.com>
2020-12-12 11:30:14 -0700

    Add place ships method

--- test/user_test.rb
+++ test/user_test.rb
@@ -9,10 +9,16 @@ class UserTest < Minitest::Test
   def setup
     @board = Board.new
     @user = User.new(@board)
+    @cruiser    = Ship.new("Cruiser", 3)
+    @submarine  = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
     assert_instance_of User, @user
     assert_instance_of Board, @user.board
+  end
+
+  def test_it_can_place_ships
+    assert_equal ["A1", "B1", "C1"], @user.place_ships(@cruiser, ["A1", "B1", "C1"])
   end
 end
