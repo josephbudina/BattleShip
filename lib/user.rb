89d326eef4b62c5ea07747fb157ba961b0f3bed2
Joseph Budina <joebud3@gmail.com>
2020-12-12 11:30:39 -0700

    Add place ships method

--- lib/user.rb
+++ lib/user.rb
@@ -4,4 +4,12 @@ class User
   def initialize(board)
     @board = board
   end
+
+  def place_ships(ship, coordinates)
+    if @board.valid_placement?(ship, coordinates)
+      @board.place(ship, coordinates)
+    else
+      "Not Valid Ship Placement"
+    end
+  end
 end
