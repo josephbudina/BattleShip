b9ce2747c512b7c4eb7b196625081f9d6463dc78
Joseph Budina <joebud3@gmail.com>
2020-12-12 10:35:19 -0700

    Add it exists test

---
+++ test/user_test.rb
@@ -0,0 +1,18 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/user'
+
+class UserTest < Minitest::Test
+  def setup
+    @board      = Board.new
+    @user   = User.new
+  end
+
+  def test_it_exists
+    assert_instance_of User, @user
+    assert_instance_of Board, @user.board
+  end
+end
