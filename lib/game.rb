e6d9db5c7a3884012fdd303ab40492afc0e95909
Joseph Budina <joebud3@gmail.com>
2020-12-12 11:06:48 -0700

    Add attributes to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,34 @@
+require './lib/computer'
+require './lib/user'
+
 class Game
     attr_reader :computer,
-                :user
+                :computer_board,
+                :user,
+                :user_board,
+                :cruiser,
+                :submarine
 
   def initialize
-    @Computer = Computer.new
-    @user = User.new
+    @computer = Computer.new(@computer_board)
+    @computer_board = Board.new
+    @user = User.new(@user_board)
+    @user_board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
 
-  def start
-    puts "     Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
-    print ">"
-    play_game = gets.chomp.downcase
+  # def start
+  #   puts "     Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
+  #   print ">"
+  #   play_game = gets.chomp.downcase
 
-    if play_game == "p"
-      # player places ships/ presented with messages
-      @computer.place_ship("Cruiser", 3)
-      @computer.place_ship("Submarine" 3)
-    elsif play_game == "q"
-    end
-  end
+  #   if play_game == "p"
+  #     # player places ships/ presented with messages
+  #     @computer.place_ship(@cruiser)
+  #     @computer.place_ship(@submarine)
+  #     puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."
+  #   elsif play_game == "q"
+  #   end
+  # end
 end
