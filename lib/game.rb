f620035432bec97c09f18f39b4d41d157ca44250
Wil McCauley <wmccauley724@gmail.com>
2020-12-14 17:10:35 -0700

    Clean up start logic and rearrange logic

--- lib/game.rb
+++ lib/game.rb
@@ -24,16 +24,23 @@ class Game
     play_game = gets.chomp.downcase
 
     if play_game == "p"
-      @computer.place_ships(@cruiser)
-      @computer.place_ships(@submarine)
+      @computer.place_ship(@cruiser)
+      @computer.place_ship(@submarine)
       puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."
       print "#{@user_board.render(true)}"
+      # @user.
+      # print "#{@computer_board.render(true)}"
       print ">"
       player_ships = gets.chomp.capitalize
-      
+      # placement_coords =
+      # player_ship_placer(player_ships)
+      coords = gets.chomp.upcase
       if player_ships == "Cruiser"
-        @user.player_ships(@cruiser)
+        @user.place_ships(@cruiser, coords.split)
       elsif player_ships == "Submarine"
+        @user.place_ships(@cruiser, gets.chomp.upcase)
+      end
+      print "#{@user_board.render(true)}"
     elsif play_game == "q"
     end
   end
